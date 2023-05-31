81cfe1bb87552de694f239573b6afc6e78c8bfd9
Denis Gorbikov <denis@gorbikov.ru>
2023-05-31 14:21:49 +0300

    Написал все 4 метода и main класс для тестирования их работы.

--- src/ProductsImpl.java
+++ src/ProductsImpl.java
@@ -7,16 +7,18 @@
  реализацию писать в теле самих функций
  вы в праве добавлять необходимые вам структуры данных в класс*/
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /*класс для реализации 4 функций*/
 public class ProductsImpl {
     // Создаёт хешмап для хранения пар id - Product.
-    HashMap<String, Product> products;
+    private HashMap<String, Product> products;
 
     // Создаём пару конуструкторов.
     public ProductsImpl() {
+        this.products = new HashMap<>();
     }
 
     public ProductsImpl(HashMap<String, Product> products) {
@@ -28,17 +30,26 @@ public class ProductsImpl {
         /*добавляет новый продукт
          возвращает true - если продукта с таким id еще не было
          возвращает false - если был такой id, вставка отменяется*/
-        /* реализация*/
-        return true;
+
+        if (!products.containsKey(product.getId())) {
+            products.put(product.getId(), product);
+            return true;
+        } else return false;
+
     }
 
 
     boolean deleteProduct(Product product) {
         /*Удаляет продукт
          возвращает true - если продукт с таким id был
-         возвроащает false - если id не было, (удаления не происходит)*/
-        /*реализация*/
-        return true;
+         возвращает false - если id не было, (удаления не происходит)*/
+
+
+        if (products.containsKey(product.getId())) {
+            products.remove(product.getId());
+            return true;
+        } else return false;
+
     }
 
 
@@ -46,8 +57,13 @@ public class ProductsImpl {
         /*Получает имя (name) продукта
          возращает name продукта у которого идентификатор равен (=) id
          если продукта нет, вернуть пустую строку ""*/
-        /*реализация*/
-        return null;
+
+
+        if (products.containsKey(id)) {
+            return products.get(id).getName();
+        } else return "";
+
+
     }
 
 
@@ -56,10 +72,28 @@ public class ProductsImpl {
          У котрых наименование равно (=) name
          Если таких нет, возвращается пустой массив (список)*/
 
-        /*реализация*/
-        return null;
+
+        List<String> ids = new ArrayList<>();
+        for (String id : products.keySet()) {
+            if (products.get(id).getName().equals(name)) {
+                ids.add(id);
+            }
+        }
+        return ids;
+
+
     }
 
+    // Добавляет для удобства тестирования toString, чтобы можно было посмотреть список products.
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("ProductsImpl: ");
+        for (String id : products.keySet()) {
+            sb.append(id + "/" + products.get(id).getName() + "; ");
+        }
+
+        return sb.toString();
+    }
 /* данный пример показан на языке dart
  Если вы будуте реализовывать на других языка - вы в праве поменять типы на аналогичные в вашей sdk
  Но только на аналогичные»*/
--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@
 
 public class main {
     public static void main(String[] args) {
-        // Создаём набор продуктов для тестирования, включая дубликат,
+        // Создаёт набор продуктов для тестирования, включая дубликат,
         // продукты с одним id и разным name, продукты с разным id но одинм name.
         Product product1 = new Product("0001", "product1");
         Product product2 = new Product("0002", "product2");
@@ -12,8 +12,70 @@ public class main {
         Product product6 = new Product("0001", "product4");
         Product product7 = new Product("0002", "product1");
 
-        // Создаём объект класса ProductsImpl для тестирования.
+        // Создаёт объект класса ProductsImpl для тестирования.
         ProductsImpl productsImpl = new ProductsImpl();
+
+
+        // Тестирует addProduct.
+        System.out.println("----------Тестирует addProduct.----------");
+        System.out.println(productsImpl.addProduct(product1));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product2));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product3));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product4));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product5));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product6));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.addProduct(product7));
+        System.out.println(productsImpl);
+        System.out.println("\n\n");
+
+        // Тестирует deleteProduct.
+        System.out.println("----------Тестирует deleteProduct.----------");
+        System.out.println(productsImpl.deleteProduct(product1));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.deleteProduct(product2));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.deleteProduct(product2));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.deleteProduct(product3));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.deleteProduct(product4));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.deleteProduct(product4));
+        System.out.println(productsImpl);
+        System.out.println("\n\n");
+
+        // Тестирует getName.
+        System.out.println("----------Тестирует getName.----------");
+        productsImpl.addProduct(product1);
+        productsImpl.addProduct(product2);
+        productsImpl.addProduct(product3);
+        productsImpl.addProduct(product4);
+        System.out.println(productsImpl);
+        System.out.println("Name: " + productsImpl.getName("0001"));
+        System.out.println("Name: " + productsImpl.getName("0002"));
+        System.out.println("Name: " + productsImpl.getName("0003"));
+        System.out.println("Name: " + productsImpl.getName("0004"));
+        System.out.println("Name: " + productsImpl.getName("0005"));
+        System.out.println("\n\n");
+
+
+        // Тестирует findByName.
+        System.out.println("----------Тестирует findByName.----------");
+        productsImpl.deleteProduct(product4);
+        productsImpl.addProduct(new Product("0004", "product3"));
+        System.out.println(productsImpl);
+        System.out.println(productsImpl.findByName("product1"));
+        System.out.println(productsImpl.findByName("product2"));
+        System.out.println(productsImpl.findByName("product3"));
+        System.out.println(productsImpl.findByName("product4"));
+        System.out.println("\n\n");
+
 
     }
 }
